bdb0d3f3f1dddf44977d6d2e07cb3a27301ae919
Kobe Li <likobe23@gmail.com>
2025-11-13 04:42:00 -0800

    Restart from zero

--- Makefile
+++ Makefile
@@ -4,21 +4,6 @@ build:
 	g++ -std=c++11 -o app \
 		main.cpp \
 		src/window.cpp \
-		src/texture.cpp \
-		src/sprite_sheet.cpp \
 		src/sdl_utils.cpp \
-		src/entities/player.cpp \
-		src/entities/enemy.cpp \
-		src/entities/entity.cpp \
-		src/entities/clickable_entity.cpp \
-		src/utils.cpp \
 		src/observers/window_observer.cpp \
-		src/resource_manager.cpp \
-		src/tiles/tileset.cpp \
-		src/tiles/tile.cpp \
-		src/startup_funcs.cpp \
-		src/cameras/camera.cpp \
-		src/cameras/zone_camera.cpp \
-		src/cameras/panning_camera.cpp \
-		src/clickable.cpp \
 		${SDL_FLAGS} -lSDL2_image -lSDL2_ttf -lSDL2_mixer
--- main.cpp
+++ main.cpp
@@ -8,18 +8,8 @@
 #include "SDL_image.h"
 
 #include "src/constants.h"
-#include "src/window.h"
-#include "src/texture.h"
-#include "src/sprite_sheet.h"
 #include "src/sdl_utils.h"
-#include "src/entities/player.h"
-#include "src/entities/enemy.h"
-#include "src/resource_manager.h"
-#include "src/tiles/tileset.h"
-#include "src/tiles/tile.h"
-#include "src/startup_funcs.h"
-#include "src/cameras/panning_camera.h"
-#include "src/cameras/zone_camera.h"
+#include "src/window.h"
 
 
 int main(int argc, char* args[]) {
@@ -34,36 +24,6 @@ int main(int argc, char* args[]) {
     }
     SDL_Renderer* renderer = window.get_renderer();
 
-    ResourceManager manager(&window);
-
-    if (!load_resources(&manager)) {
-        return -1;
-    }
-
-    Texture* full_overworld = manager.get_texture("resources/overworld__full.png");
-
-    std::unique_ptr<Camera> _camera(new ZoneCamera(7 * NES_SCREEN_WIDTH, 7 * NES_SCREEN_HEIGHT, &window));
-    Camera& camera = *_camera;
-    
-    Player player = Player(
-        manager.get_sprite("resources/sprites/link.png"),
-        &window,
-        _camera.get()
-    );
-    player.set_x(7 * NES_SCREEN_WIDTH);
-    player.set_y(7 * NES_SCREEN_HEIGHT);
-
-    Enemy enemy = Enemy(
-        manager.get_sprite("resources/sprites/oktorok__red.png"),
-        &window,
-        _camera.get()
-    );
-    enemy.set_x(7 * NES_SCREEN_WIDTH + NES_SCREEN_WIDTH / 2);
-    enemy.set_y(7 * NES_SCREEN_HEIGHT + NES_SCREEN_HEIGHT / 2);
-
-    std::vector<SDL_Rect*> colliders;
-    colliders.push_back(&enemy.get_collider());
-
     SDL_Event e;
     int running = 1;
 
@@ -74,29 +34,10 @@ int main(int argc, char* args[]) {
             }
 
             window.handle_event(e);
-            camera.handle_event(e);
-            player.handle_event(e);
-            enemy.handle_event(e);
         }
-
-        camera.move();
-        player.move(colliders);
-
-        // Zone current_zone = camera.get_current_zone();
-        // printf("currently in zone %d-%d\n", current_zone.col, current_zone.row);
 
         SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
         SDL_RenderClear(renderer);
-
-        full_overworld->render(
-            0 - camera.get_x(),
-            0 - camera.get_y(),
-            NULL,
-            camera.get_scale_x(),
-            camera.get_scale_y()
-        );
-        player.render(camera);
-        enemy.render(camera);
 
         SDL_RenderPresent(renderer);
     }
