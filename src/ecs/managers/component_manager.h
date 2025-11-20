b6cf18a77b095ffb612c2ee3c7729de363ebda65
Kobe Li <likobe23@gmail.com>
2025-11-19 20:48:35 -0800

    Refactor ComponentManager to remove type erasure

--- src/ecs/managers/component_manager.h
+++ src/ecs/managers/component_manager.h
@@ -8,53 +8,70 @@
 #include "../entity.h"
 
 
+class IComponentStore {
+public:
+    virtual ~IComponentStore() = default;
+    virtual void remove_component(int entity_id) = 0;
+};
+
+
+template <typename T>
+class ComponentStore : public IComponentStore {
+public:
+    T* add_component(Entity entity, std::unique_ptr<T> component) {
+        m_components[entity.get_id()] = std::move(component);
+        return m_components[entity.get_id()].get();
+    }
+
+    T* get_component(Entity entity) {
+        if (m_components.find(entity.get_id()) == m_components.end()) {
+            return NULL;
+        }
+
+        return m_components[entity.get_id()].get();
+    }
+
+    void remove_component(int entity_id) override {
+        m_components.erase(entity_id);
+    }
+private:
+    std::unordered_map<int, std::unique_ptr<T>> m_components;
+};
+
+
 class ComponentManager {
 public:
     template <typename T>
     T* add_component(Entity entity, std::unique_ptr<T> component) {
-        std::map<int, std::unique_ptr<T> >& component_map = get_map<T>();
-    
-        if (component_map.find(entity.get_id()) != component_map.end()) {
-            printf("Entity with id %d already has component of type %s\n", entity.get_id(), typeid(T).name());
-            return NULL;
-        }
+        ComponentStore<T>& component_store = get_or_create_store<T>();
+        component_store.add_component(entity, std::move(component));
 
-        component_map[entity.get_id()] = std::move(component);
-
-        return component_map[entity.get_id()].get();
+        return component_store.get_component(entity);
     }
 
     template <typename T>
     T* get_component(Entity entity) {
-        std::map<int, std::unique_ptr<T> >& component_map = get_map<T>();
+        ComponentStore<T>& component_store = get_or_create_store<T>();
 
-        if (component_map.find(entity.get_id()) == component_map.end()) {
-            return NULL;
-        }
-
-        return component_map[entity.get_id()].get();
+        return component_store.get_component(entity);
     }
 
 private:
-    /**
-     * Type erased mapping of `Type -> Map<Entity, Component>`
-    ```
-    std::unordered_map< std::type_index, std::shared_ptr<std::map<int, std::unique_ptr<T>>>
-    ```
-     */
-    std::unordered_map<std::type_index, std::shared_ptr<void> > m_component_maps;
+    std::unordered_map<std::type_index, std::unique_ptr<IComponentStore>> m_stores;
 
     template <typename T>
-    std::map<int, std::unique_ptr<T> >& get_map() {
+    ComponentStore<T>& get_or_create_store() {
         std::type_index type = std::type_index(typeid(T));
 
-        if (m_component_maps.find(type) == m_component_maps.end()) {
-            // map doesn't exist; create it
-            auto new_map = std::make_shared<std::map<int, std::unique_ptr<T>>>();
-            m_component_maps[type] = std::move(new_map);
+        if (m_stores.find(type) == m_stores.end()) {
+            std::unique_ptr<ComponentStore<T>> store (new ComponentStore<T>());
+            ComponentStore<T>* store_ptr = store.get();
+            m_stores[type] = std::move(store);
+
+            return *store_ptr;
         }
 
-        return *std::static_pointer_cast< std::map<int, std::unique_ptr<T> > >(m_component_maps[type]);
+        return *static_cast<ComponentStore<T>*>(m_stores[type].get());
     }
 };
 
